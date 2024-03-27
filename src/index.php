e08b150e83710ef3933aaa112c47acd15c385a0d
Axodouble <53979495+Axodouble@users.noreply.github.com>
2024-03-27 12:45:49 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 class catImage # Purely a class because I want to add the ability to add image credits in the future and to add titles for example.
 {
     public $url;
+    public $description;
     public function __construct($url)
     {
         $this->url = $url;
@@ -12,31 +13,40 @@ function getCat()
 {
     $cats = array(
         new catImage(
-            "https://www.marthastewart.com/thmb/g-FunKfdiZombJQ7pB4wb8BF4C8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cat-kitten-138468381-4cd82b91d7be45cb9f9aa8366e10bce4.jpg"
+            "https://www.marthastewart.com/thmb/g-FunKfdiZombJQ7pB4wb8BF4C8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cat-kitten-138468381-4cd82b91d7be45cb9f9aa8366e10bce4.jpg",
+            "",
         ),
         new catImage(
-            "https://d2zp5xs5cp8zlg.cloudfront.net/image-78806-800.jpg"
+            "https://d2zp5xs5cp8zlg.cloudfront.net/image-78806-800.jpg",
+            "",
         ),
         new catImage(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT8ZSEWwF0rsqs4qiGRukSJewgQrvyChibZw&s"
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT8ZSEWwF0rsqs4qiGRukSJewgQrvyChibZw&s",
+            "",
         ),
         new catImage(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2hIoAk-gqzO5hesR1ghHAksOJ-y5CsJAfjg&s"
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2hIoAk-gqzO5hesR1ghHAksOJ-y5CsJAfjg&s",
+            "",
         ),
         new catImage(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVJCpCdWiUEXEM_XdSnqHcvfZkz3U5NOZLMQ&s"
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVJCpCdWiUEXEM_XdSnqHcvfZkz3U5NOZLMQ&s",
+            "",
         ),
         new catImage(
-            "https://media-be.chewy.com/wp-content/uploads/temperament_kittens_Nov_RR_SB_LC.jpg"
+            "https://media-be.chewy.com/wp-content/uploads/temperament_kittens_Nov_RR_SB_LC.jpg",
+            "",
         ),
         new catImage(
-            "https://ahs.nyc3.cdn.digitaloceanspaces.com/live/public/styles/crop_16_9_960x540/public/media/image/2022-06/group-of-kittens.jpg?h=df37f6f0&itok=g3ayM11z"
+            "https://ahs.nyc3.cdn.digitaloceanspaces.com/live/public/styles/crop_16_9_960x540/public/media/image/2022-06/group-of-kittens.jpg?h=df37f6f0&itok=g3ayM11z",
+            "",
         ),
         new catImage(
-            "https://tenlives.com.au/wp-content/uploads/2020/09/Found-Kitten-0-8-Weeks-Quiet-scaled.jpg"
+            "https://tenlives.com.au/wp-content/uploads/2020/09/Found-Kitten-0-8-Weeks-Quiet-scaled.jpg",
+            "My personal favorite.",
         ),
         new catImage(
-            "https://d2zp5xs5cp8zlg.cloudfront.net/image-61209-800.jpg"
+            "https://d2zp5xs5cp8zlg.cloudfront.net/image-61209-800.jpg",
+            "",
         ),
     );
     return $cats[array_rand($cats)];
@@ -60,6 +70,7 @@ function loadPage()
                 <h1>lil.cat</h1>
             </a>
             <img src="<?php echo $cat->url; ?>" alt="lil.cat">
+            <a><?php echo $cat->$description; ?></a>
         </div>
     </body>
 
